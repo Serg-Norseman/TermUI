df628b7dc161d34509df82114abf35d2c22909c4
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2026-03-06 00:52:39 +0300

    Added NumericStepper

---
+++ Terminal.Gui/Views/NumericStepper.cs
@@ -0,0 +1,111 @@
+//
+// NumericStepper.cs: NumericStepper control
+//
+// Authors:
+//   Serg V. Zhdanovskikh
+//
+
+using System;
+
+namespace Terminal.Gui {
+	public class NumericStepper : View {
+		private readonly TextField _textField;
+		private int _value;
+
+		public int Maximum { get; set; } = int.MaxValue;
+
+		public int Minimum { get; set; } = int.MinValue;
+
+		public int Step { get; set; } = 1;
+
+		public int Value {
+			get { return _value; }
+			set {
+				_value = Math.Max (Minimum, Math.Min (Maximum, value));
+				_textField.Text = _value.ToString ();
+				ValueChanged?.Invoke (this, EventArgs.Empty);
+			}
+		}
+
+
+		public event EventHandler ValueChanged;
+
+
+		public NumericStepper () : base ()
+		{
+			Height = 1;
+			//CanFocus = true;
+
+			_textField = new TextField () {
+				TextAlignment = TextAlignment.Right
+			};
+			_textField.Leave += (e) => {
+				if (int.TryParse (_textField.Text.ToString (), out var v)) {
+					Value = v;
+				} else {
+					_textField.Text = Value.ToString ();
+				}
+			};
+			Add (_textField);
+
+			// On resize
+			LayoutComplete += (LayoutEventArgs a) => {
+				var bounds = Bounds;
+				if (bounds.Width > 0) {
+					_textField.X = 0;
+					_textField.Y = 0;
+					_textField.Width = bounds.Width - 2;
+					//_textField.SetRelativeLayout (Bounds);
+				}
+			};
+		}
+
+		///<inheritdoc/>
+		public override void Redraw (Rect bounds)
+		{
+			base.Redraw (bounds);
+
+			Driver.SetAttribute (GetNormalColor ());
+			//Driver.SetAttribute (ColorScheme.Focus);
+
+			Move (bounds.Right - 2, 0);
+			Driver.AddRune (Driver.DownArrow);
+
+			Move (bounds.Right - 1, 0);
+			Driver.AddRune (Driver.UpArrow);
+		}
+
+		///<inheritdoc/>
+		public override bool MouseEvent (MouseEvent me)
+		{
+			if (me.Y == Bounds.Top && me.Flags == MouseFlags.Button1Pressed) {
+				if (me.X == Bounds.Right - 2) {
+					Value = Value - Step;
+					return true;
+				} else if (me.X == Bounds.Right - 1) {
+					Value = Value + Step;
+					return true;
+				}
+			} else if (me.Flags == MouseFlags.Button1Pressed) {
+				if (!_textField.HasFocus) {
+					_textField.SetFocus ();
+				}
+				return true;
+			}
+			return false;
+		}
+
+		public override bool ProcessKey (KeyEvent kb)
+		{
+			if (kb.Key == Key.CursorUp) {
+				Value += Step;
+				return true;
+			}
+			if (kb.Key == Key.CursorDown) {
+				Value -= Step;
+				return true;
+			}
+			return base.ProcessKey (kb);
+		}
+	}
+}
---
+++ UICatalog/Scenarios/NumericStepperExample.cs
@@ -0,0 +1,27 @@
+﻿using Terminal.Gui;
+
+namespace UICatalog.Scenarios {
+	[ScenarioMetadata (Name: "NumericStepper", Description: "Demo for NumericStepper")]
+	[ScenarioCategory ("Controls"), ScenarioCategory ("NumericStepper")]
+	public class NumericStepperExample : Scenario {
+		public override void Setup ()
+		{
+			var lblNum = new Label () {
+				X = 10,
+				Y = 10,
+				Text = "NumericStepper"
+			};
+
+			var numStepper = new NumericStepper () {
+				X = 10,
+				Y = 11,
+				Width = 20,
+				Minimum = 0,
+				Maximum = 70,
+				Step = 1,
+				Value = 7,
+			};
+			Win.Add (lblNum, numStepper);
+		}
+	}
+}
